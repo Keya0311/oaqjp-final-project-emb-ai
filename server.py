7182d8624bb30b6db764ab95fb676ef270597a41
keya0311 <keya.flower@gmail.com>
2026-05-14 13:39:57 -0400

    updated server.py for static code analysis using pylint library

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request 
-from EmotionDetection.emotion_detection  import emotion_detector
+"""Simple Flask API server."""
+from flask import Flask,render_template,request
+from EmotionDetection.emotion_detection import emotion_detector
 app=Flask("Emotion Detector")
 @app.route("/emotionDetector")
 def sent_analyzer():
@@ -12,24 +13,21 @@ def sent_analyzer():
     response = emotion_detector(text_to_analyze)
     if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
-    else:
-        # Return a formatted string with the sentiment label and score
-        return (
-        f"For the given statement, the system response is "
-        f"'anger': {response['anger']}, "
-        f"'disgust': {response['disgust']}, "
-        f"'fear': {response['fear']}, "
-        f"'joy': {response['joy']} and "
-        f"'sadness': {response['sadness']}. "
-        f"The dominant emotion is {response['dominant_emotion']}."
-    )
+
+    return (
+    f"For the given statement, the system response is "
+    f"'anger': {response['anger']}, "
+    f"'disgust': {response['disgust']}, "
+    f"'fear': {response['fear']}, "
+    f"'joy': {response['joy']} and "
+    f"'sadness': {response['sadness']}. "
+    f"The dominant emotion is {response['dominant_emotion']}."
+)
 @app.route("/")
 def render_index_page():
     ''' This function initiates the rendering of the main application
         page over the Flask channel
     '''
     return render_template("index.html")
-    #TODO
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=8080)
+    app.run(host="0.0.0.0",port=5000)
